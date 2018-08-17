6192a78142fead57692bfbd32c4b0f521a62829a
wyrover <wyrover@gmail.com>
2018-08-17 12:00:51 +0800

    + 3rdparty

---
+++ 3rdparty/CuteLogger/include/CuteLogger_global.h
@@ -0,0 +1,18 @@
+#ifndef CUTELOGGER_GLOBAL_H
+#define CUTELOGGER_GLOBAL_H
+
+#include <QtCore/qglobal.h>
+
+#if defined(CUTELOGGER_LIBRARY)
+    #define CUTELOGGERSHARED_EXPORT
+#else
+    #define CUTELOGGERSHARED_EXPORT
+#endif
+
+//#if defined(CUTELOGGER_LIBRARY)
+//#  define CUTELOGGERSHARED_EXPORT Q_DECL_EXPORT
+//#else
+//#  define CUTELOGGERSHARED_EXPORT Q_DECL_IMPORT
+//#endif
+
+#endif // CUTELOGGER_GLOBAL_H
