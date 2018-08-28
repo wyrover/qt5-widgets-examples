28f3718d68d98dcde83aa2fc8f406d8aee949207
wyrover <wyrover@gmail.com>
2018-08-28 10:29:55 +0800

    + src

---
+++ src/tagbuttton/main.cpp
@@ -0,0 +1,32 @@
+#include "widget.h"
+#include <QApplication>
+#include <QFile>
+
+#include <QtPlugin>
+
+#if defined(Q_OS_WIN32)
+Q_IMPORT_PLUGIN(QWindowsIntegrationPlugin)
+#elif defined(Q_OS_MAC)
+Q_IMPORT_PLUGIN(QCocoaIntegrationPlugin)
+#endif
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+
+	//QFile styleFile(":/styles/styles/default.qss");
+
+	QFile styleFile("styles/default.qss");
+
+	styleFile.open(QFile::ReadOnly);
+	// Apply the loaded stylesheet
+	QString style(styleFile.readAll());
+	a.setStyleSheet(style);
+
+
+    Widget w;
+    w.show();
+
+    return a.exec();
+}
